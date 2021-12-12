2d8fd13d3b8c01eb6ee031ea4f3e072db25064e0
AbdulrahmanY <abdulrahmansaddiq@gmail.com>
2021-12-12 09:30:20 +0300

    setup for backend

---
+++ src/test/java/com/example/prolect4_test1/Prolect4Test1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.prolect4_test1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Prolect4Test1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
